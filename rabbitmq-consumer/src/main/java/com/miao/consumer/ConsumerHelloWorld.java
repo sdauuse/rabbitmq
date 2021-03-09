d70a65a0b207946918c8f074e22e44348742d7fc
sdauuse <751648872@qq.com>
2021-03-09 13:52:22 +0800

    Initial commit

---
+++ rabbitmq-consumer/src/main/java/com/miao/consumer/ConsumerHelloWorld.java
@@ -0,0 +1,12 @@
+package com.miao.consumer;
+
+/**
+ * @author miaoyin
+ * @date 2021/3/9 - 13:36
+ * @commet:
+ */
+public class ConsumerHelloWorld {
+    public static void main(String[] args) {
+
+    }
+}
