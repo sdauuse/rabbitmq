d8f70160b953c3b7c591aa64ad08643e2893b8a5
sdauuse <751648872@qq.com>
2021-03-10 15:31:07 +0800

    新增rabbitmq整合springboot

--- spring-rabbitmq-producer/src/test/java/com/miao/producer/SpringProducer.java
+++ spring-rabbitmq-producer/src/test/java/com/miao/producer/SpringProducer.java
@@ -2,6 +2,7 @@ package com.miao.producer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -26,5 +27,40 @@ public class SpringProducer {
         rabbitTemplate.convertAndSend("spring_queue","hello spring-rabbit................");
     }
 
+    /**
+     * 确认模式：
+     * 步骤：
+     * 1. 确认模式开启：spring配置文件中设置，ConnectionFactory中开启publisher-confirms="true"
+     * 2. 在rabbitTemplate定义ConfirmCallBack回调函数
+     */
+    @Test
+    public void testConfirm() {
+
+        //2. 定义回调
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            /**
+             *
+             * @param correlationData 相关配置信息
+             * @param ack   exchange交换机 是否成功收到了消息。true 成功，false代表失败
+             * @param cause 失败原因
+             */
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                System.out.println("confirm方法被执行了....");
+
+                if (ack) {
+                    //接收成功
+                    System.out.println("接收成功消息" + cause);
+                } else {
+                    //接收失败
+                    System.out.println("接收失败消息" + cause);
+                    //做一些处理，让消息再次发送。
+                }
+            }
+        });
+
+        //3. 发送消息
+        rabbitTemplate.convertAndSend("test_exchange_confirm", "confirm", "message confirm....");
+    }
 
 }
